be6cbc92c997c873b6a014daf80baa0d762de715
AleksandraKasperowicz <aleksandrakasperowicz@wp.pl>
2023-02-08 13:02:52 +0100

    Add test to main for app class

--- app/src/test/groovy/pl/futurecollars/invoicing/AppTest.groovy
+++ app/src/test/groovy/pl/futurecollars/invoicing/AppTest.groovy
@@ -11,18 +11,20 @@ class AppTest extends Specification {
         def app = new App()
 
         when:
-        def result = app.greeting
+        def result = app.getGreeting()
 
         then:
         result != null
-    }
-
-    def "dummy test to cover main"() {
-        setup:
-        def app = new App()
 
         and:
         app.main()
-
     }
+
+//    def "dummy test to cover main"() {
+//        setup:
+//        def app = new App()
+//
+//        and:
+//        app.main()
+
 }

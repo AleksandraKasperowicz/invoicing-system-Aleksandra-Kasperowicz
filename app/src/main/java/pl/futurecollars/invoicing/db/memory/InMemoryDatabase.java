4c832bd04ff2dee243840584cd5f4a3be89663cc
AleksandraKasperowicz <aleksandrakasperowicz@wp.pl>
2023-02-12 13:48:12 +0100

    Add implements InMemoryDatabase

--- app/src/main/java/pl/futurecollars/invoicing/db/memory/InMemoryDatabase.java
+++ app/src/main/java/pl/futurecollars/invoicing/db/memory/InMemoryDatabase.java
@@ -1,4 +1,33 @@
 package pl.futurecollars.invoicing.db.memory;
 
-public class InMemoryDatabase {
+import java.util.List;
+import java.util.Optional;
+import pl.futurecollars.invoicing.db.DataBase;
+import pl.futurecollars.invoicing.model.Invoice;
+
+public class InMemoryDatabase implements DataBase {
+  @Override
+  public int save(Invoice invoice) {
+    return 0;
+  }
+
+  @Override
+  public Optional<Invoice> getById(int id) {
+    return Optional.empty();
+  }
+
+  @Override
+  public List<Invoice> getAll() {
+    return null;
+  }
+
+  @Override
+  public void update(int id, Invoice updatedInvoice) {
+
+  }
+
+  @Override
+  public void delete(int id) {
+
+  }
 }

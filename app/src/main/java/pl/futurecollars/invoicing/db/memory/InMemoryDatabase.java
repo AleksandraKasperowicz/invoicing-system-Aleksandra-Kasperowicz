f2368c14d371c5701fad057cd98ae18de14dc77e
AleksandraKasperowicz <aleksandrakasperowicz@wp.pl>
2023-02-13 14:05:46 +0100

    Add implements InMemoryDatabase

--- app/src/main/java/pl/futurecollars/invoicing/db/memory/InMemoryDatabase.java
+++ app/src/main/java/pl/futurecollars/invoicing/db/memory/InMemoryDatabase.java
@@ -1,33 +1,47 @@
 package pl.futurecollars.invoicing.db.memory;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import pl.futurecollars.invoicing.db.DataBase;
 import pl.futurecollars.invoicing.model.Invoice;
 
 public class InMemoryDatabase implements DataBase {
+
+  final Map<Integer, Invoice> invoices = new HashMap<>();
+  private int currentId = 0;
   @Override
   public int save(Invoice invoice) {
-    return 0;
+    invoice.setId(getNextId());
+    invoices.put(invoice.getId(),invoice);
+    return invoice.getId();
   }
 
   @Override
   public Optional<Invoice> getById(int id) {
-    return Optional.empty();
+    return Optional.ofNullable(invoices.get(id));
   }
 
   @Override
   public List<Invoice> getAll() {
-    return null;
+    return new ArrayList<>(invoices.values());
   }
 
   @Override
   public void update(int id, Invoice updatedInvoice) {
-
+    if(!invoices.containsKey(id)) {
+      throw new IllegalArgumentException("Faktura o numerze: " + id + "nie istnieje");
+    }
+    updatedInvoice.setId(id);
+    invoices.put(id, updatedInvoice);
   }
-
   @Override
   public void delete(int id) {
-
+    invoices.remove(id);
+  }
+  private int getNextId(){
+    return ++currentId;
   }
 }
--- app/src/main/java/pl/futurecollars/invoicing/service/InvoiceService.java
+++ app/src/main/java/pl/futurecollars/invoicing/service/InvoiceService.java
@@ -1,4 +1,50 @@
 package pl.futurecollars.invoicing.service;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import pl.futurecollars.invoicing.db.DataBase;
+import pl.futurecollars.invoicing.model.Invoice;
+
 public class InvoiceService {
+
+  private final DataBase database;
+
+  public InvoiceService(DataBase database) {
+    this.database = database;
+  }
+
+  public int save(Invoice invoice) {
+    return database.save(invoice);
+  }
+
+  public Optional<Invoice> getById(int id) {
+    return database.getById(id);
+  }
+
+  public List<Invoice> getAll() {
+    return database.getAll();
+  }
+
+  public void update(int id, Invoice updatedInvoice) {
+    database.update(id, updatedInvoice);
+  }
+
+  public void delete(int id) {
+    database.delete(id);
+  }
+  public List<Invoice> getInvoicesByBuyer(String buyer){
+    List<Invoice> allInvoices = getAll();
+    return allInvoices
+        .stream()
+        .filter(i -> i.getBuyer().equals(buyer))
+        .collect(Collectors.toList());
+  }
+  public List<Invoice> getInvoicesBySeller(String seller){
+    List<Invoice> allInvoices = getAll();
+    return allInvoices
+        .stream()
+        .filter(i -> i.getSeller().equals(seller))
+        .collect(Collectors.toList());
+  }
 }

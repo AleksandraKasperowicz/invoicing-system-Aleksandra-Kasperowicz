9e4f921f2c19af6289bd424b5f78056820aacfe3
AleksandraKasperowicz <aleksandrakasperowicz@wp.pl>
2023-03-11 11:13:52 +0100

    add change import ass class

--- app/src/main/java/pl/futurecollars/invoicing/App.java
+++ app/src/main/java/pl/futurecollars/invoicing/App.java
@@ -1,7 +1,7 @@
 package pl.futurecollars.invoicing;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class App {

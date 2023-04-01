0e582b0d31c78d364acdbf820422679e3219d8bc
AleksandraKasperowicz <aleksandrakasperowicz@wp.pl>
2023-04-01 19:16:03 +0200

    move class SpringFoxConfig to package configuration

--- app/src/main/java/pl/futurecollars/invoicing/configuration/SpringFoxConfig.java
+++ app/src/main/java/pl/futurecollars/invoicing/configuration/SpringFoxConfig.java
@@ -1,4 +1,4 @@
-package pl.futurecollars.invoicing.controller;
+package pl.futurecollars.invoicing.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;

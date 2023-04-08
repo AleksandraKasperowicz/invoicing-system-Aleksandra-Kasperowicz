d63c2f2df16a36ee79332415b2a4062ff81e9c2e
AleksandraKasperowicz <aleksandrakasperowicz@wp.pl>
2023-04-08 10:39:00 +0200

    update task 10

--- app/src/test/groovy/pl/futurecollars/invoicing/controller/AbstractControllerTest.groovy
+++ app/src/test/groovy/pl/futurecollars/invoicing/controller/AbstractControllerTest.groovy
@@ -6,8 +6,8 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.ResultActions
 import pl.futurecollars.invoicing.model.Invoice
+import pl.futurecollars.invoicing.model.Company
 import pl.futurecollars.invoicing.service.CalculatorResult
 import spock.lang.Specification
 
